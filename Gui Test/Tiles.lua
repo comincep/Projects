cd7ce7956c90fd1b3521bf7872ed75c5d3fb0934
comincep <comincep@gmail.com>
2024-10-13 20:08:50 +0100

    Added Anti-Crash and fixed a minor bug (also added keyboard controls and mouse controls (WIP) but you need to enable them)

--- Gui Test/Tiles.lua
+++ Gui Test/Tiles.lua
@@ -5,13 +5,13 @@ _G.Tiles = {}
 
 function Tiles:Load()
 
-  self.x = 0
-  self.y = 0
+  self.x = 50
+  self.y = 50
 
-  self.height = 50
-  self.width = 50
+  self.height = 100
+  self.width = 100
 
-  self.TileCount = 0
+  self.TileCount = {}
 
   self.Debounce = false
 
@@ -20,6 +20,8 @@ end
 
 
 function Tiles:Update(dt)
+  Tiles:KeyboardKeys()
+  Tiles:Mouse()
 
 end
 
@@ -27,27 +29,61 @@ end
 
 function Tiles:Draw()
 
-  Tiles:NewTiles(1, 1)
-  love.graphics.print("Tiles: ".. self.TileCount)
+  Tiles:NewTiles(8, 6)
+  love.graphics.print("Tiles: ".. #self.TileCount)
 
 end
 
 
 function Tiles:NewTiles(VerNT, HorNT)
+  if VerNT and HorNT < 50 then
   for i = 1, VerNT do
-    rectangle("line", self.x * i, self.y, self.width * i, self.height )
+    rectangle("line", self.x, self.y, self.width * i, self.height )
 
     for ii = 1, HorNT do
-      rectangle("line", self.x * i, self.y * ii, self.width * i, self.height * ii)
+      rectangle("line", self.x, self.y, self.width * i, self.height * ii)
 
       if self.Debounce == false then
 
-        self.TileCount = self.TileCount + 1
+        table.insert(self.TileCount, ii, ii)
+        end
       end
     end
-  end
-  
+
   self.Debounce = true
+else
+  print("NO CRASHING ALLOWED")
+  end
+end
+
+
+function Tiles:KeyboardKeys()
+
+  if love.keyboard.isDown("right") then
+    self.x = self.x + 10
+  end
+
+  if love.keyboard.isDown("left") then
+    self.x = self.x - 10
+  end
+
+  if love.keyboard.isDown("up") then
+    self.y = self.y - 10
+  end
+
+  if love.keyboard.isDown("down") then
+    self.y = self.y + 10
+  end
+
+end
+
+
+function Tiles:Mouse() -- FIX ME IM DYINGGG
+
+  if love.mouse.isDown(1) then
+    self.x = love.mouse.getX() / 8
+    self.y = love.mouse.getY() / 6
+  end
 end
 
 
